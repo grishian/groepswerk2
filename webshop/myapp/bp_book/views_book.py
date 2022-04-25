c5b3d25b31ed32ed6e78569eaa4b633fdfc7696e
grishian <grishian@hotmail.com>
2022-04-25 16:19:50 +0200

    pauze

--- webshop/myapp/bp_book/views_book.py
+++ webshop/myapp/bp_book/views_book.py
@@ -1,7 +1,11 @@
 from myapp.bp_book import bp_book
+from myapp.bp_book.model_book import Book
 from flask import render_template
 
 
-@bp_book.route('/book')
-def do_book():
-    return render_template('book/_book.html')
+@bp_book.route('/book/<isbn>')
+def do_book(isbn):
+
+    book = Book.query.filter_by(isbn=isbn).first_or_404()
+
+    return render_template('book/_book.html', book=book)
