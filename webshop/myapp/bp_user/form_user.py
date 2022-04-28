9768953ee02bf968f79790aafee4ad253b02af84
grishian <grishian@hotmail.com>
2022-04-28 15:14:48 +0200

    registration form, page added

--- webshop/myapp/bp_user/form_user.py
+++ webshop/myapp/bp_user/form_user.py
@@ -7,3 +7,10 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', id='login_password')
     remember_me = BooleanField('Keep me logged in', id='remember_me')
     submit = SubmitField('Log In', id='login_submit')
+
+
+class SignUpForm(FlaskForm):
+    email = StringField('Email', id='signup_email')
+    username = StringField('Username', id='signup_username')
+    password = PasswordField('Password', id='signup_password')
+    submit = SubmitField('Submit', id='signup_submit')
--- webshop/myapp/bp_user/views_user.py
+++ webshop/myapp/bp_user/views_user.py
@@ -4,7 +4,7 @@ from flask_login import login_required, current_user, login_user, logout_user
 from myapp import db
 from myapp.bp_user import bp_user
 from myapp.bp_user.model_user import User, only_admins
-from myapp.bp_user.form_user import LoginForm
+from myapp.bp_user.form_user import LoginForm, SignUpForm
 
 
 @bp_user.route('/user')
@@ -63,3 +63,26 @@ def do_logout():
         flash('You are now logged out', 'OK')
         logout_user()
         return redirect(url_for('bp_general.do_home'))
+
+
+@bp_user.route('/register', methods=['GET', 'POST'])
+def do_register():
+    user = current_user
+    if user and user.is_authenticated:
+        flash('You are already logged in, you can not signup', 'WARNING')
+        return redirect(url_for('bp_general.do_home'))
+
+    form = SignUpForm()
+    if form.validate_on_submit():
+        user = User()
+        user.username = form.username.data
+        user.email = form.email.data
+        user.set_password(form.password.data)
+        user.profile_type = 1
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('bp_user.do_login', user_id=user.id))
+
+    return render_template('user/register.html', form=form)
