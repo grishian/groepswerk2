ac7cb1db78da36a367051a6637abd3bc0a06a275
grishian <grishian@hotmail.com>
2022-04-30 09:58:03 +0200

    You can now see your added books to wishlist(no duplicates allowed)

--- webshop/main.py
+++ webshop/main.py
@@ -2,6 +2,7 @@ import sys
 from myapp import create_app, db
 from myapp.bp_book.model_book import Book
 from myapp.bp_user.model_user import User
+from myapp.bp_wishlist.model_wishlist import Wishlist
 
 sys.dont_write_bytecode = True
 
@@ -11,7 +12,8 @@ app = create_app()
 def make_shell_context():
     return {'db': db,
             'Book': Book,
-            'User': User}
+            'User': User,
+            'Wishlist': Wishlist}
 
 
 if __name__ == "__main__":
--- webshop/myapp/bp_wishlist/views_wishlist.py
+++ webshop/myapp/bp_wishlist/views_wishlist.py
@@ -1,6 +1,6 @@
 from myapp import db
 from myapp.bp_wishlist import bp_wishlist
-from flask import render_template, url_for, flash
+from flask import render_template, url_for, flash, redirect
 from flask_login import login_required, current_user
 from myapp.bp_wishlist.model_wishlist import Wishlist
 from myapp.bp_book.model_book import Book
@@ -8,7 +8,16 @@ from myapp.bp_book.model_book import Book
 
 @bp_wishlist.route('/wishlist')
 def do_wishlist():
-    return render_template('wishlist/wishlist.html')
+    my_wishlist_book_ids = []
+    my_wishlist_items = Wishlist.query.filter_by(user_id=current_user.id).all()
+    for wishlist_item in my_wishlist_items:
+        my_wishlist_book_ids.append(wishlist_item.book_id)
+
+    books = []
+    for book_id in my_wishlist_book_ids:
+        books.append(Book.query.get(book_id))
+
+    return render_template('wishlist/wishlist.html', books=books)
 
 
 @bp_wishlist.route('/add_to_my_wishlist/<isbn>', methods=["GET", "POST"])
@@ -17,18 +26,25 @@ def add_to_wishlist(isbn):
 
     book = Book.query.filter_by(isbn=isbn).first_or_404()
     user_id = current_user.id
+    my_wishlist_book_ids = []
+    my_wishlist_items = Wishlist.query.filter_by(user_id=user_id).all()
+    for wishlist_item in my_wishlist_items:
+        my_wishlist_book_ids.append(wishlist_item.book_id)
+    if not isbn in my_wishlist_book_ids:
 
-    wishlist_item = Wishlist()
-    wishlist_item.book_id = isbn
-    wishlist_item.user_id = user_id
+        wishlist_item = Wishlist()
+        wishlist_item.book_id = isbn
+        wishlist_item.user_id = user_id
 
-    db.session.add(wishlist_item)
-    db.session.commit()
+        db.session.add(wishlist_item)
+        db.session.commit()
 
+        flash('Book: {} added to your wishlist'.format(book.title), 'OK')
+        return redirect(url_for('bp_wishlist.do_wishlist'))
 
-    flash('Book:{} added to your wishlist'.format(book.title), 'OK')
+    flash('Book: {} already in your wishlist'.format(book.title), 'ERROR')
+    return redirect(url_for('bp_wishlist.do_wishlist'))
 
-    return render_template('wishlist/wishlist.html')
 
 
 
